acae9f7d6f0370afff9fe12cc2a33b174c3eeb99
Guna-Pandi <gunapandidurai@gmail.com>
2024-12-13 16:03:10 +0530

    img

--- src/components/Portrait/Portrait.jsx
+++ src/components/Portrait/Portrait.jsx
@@ -3,29 +3,29 @@ import "./Portrait.css";
 
 const Portrait = () => {
    // State to toggle between the two images
-   const [currentImage, setCurrentImage] = useState(0);
+   // const [currentImage, setCurrentImage] = useState(0);
 
-   // Array of background images
-   const images = [
-      "/assets/photos/main-img2.png",
-      "/assets/photos/main-pic.png"
-   ];
+   // // Array of background images
+   // const images = [
+   //    "/assets/photos/main-img2.png",
+   //    "/assets/photos/main-pic.png"
+   // ];
 
-   useEffect(() => {
-      // Switch images every 2 seconds
-      const interval = setInterval(() => {
-         setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-      }, 1000);
+   // useEffect(() => {
+   //    // Switch images every 2 seconds
+   //    const interval = setInterval(() => {
+   //       setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+   //    }, 1000);
 
-      // Cleanup the interval on component unmount
-      return () => clearInterval(interval);
-   }, [images.length]);
+   //    // Cleanup the interval on component unmount
+   //    return () => clearInterval(interval);
+   // }, [images.length]);
 
    return (
       <div
          className="main-pic"
          style={{
-            backgroundImage: `url(${images[currentImage]})`,
+            backgroundImage: `url("/assets/photos/Main-pic.jfif")`,
          }}
       ></div>
    );
--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import './Spinner.css'
 const Spinner = () => {
    return (
 
-      <div className=" h-[200px] w-[200px] absolute top-[15px] -left-[40px] z-50 flex justify-center items-center lg:top-2 lg:left-4 ">
+      <div className=" h-[180px] w-[180px] absolute top-[15px] -left-[40px] z-50 flex justify-center items-center lg:top-1 lg:left-4 ">
 <div></div>
          <img
             src="/assets/images/rounded-text.png"

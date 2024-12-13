33f0199c2ba329adbc2e15d9ddac4f85d93627c1
Guna-Pandi <gunapandidurai@gmail.com>
2024-12-13 16:20:12 +0530

    spinner

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,12 +3,12 @@ const Carousel = () => {
 
    return (
       <section className='flex justify-center items-baseline relative z-40 w-[400px] h-72 pt-12 overflow-hidden
-         lg:w-full lg:h-96 lg:pt-6 '>
-         < img src="/assets/photos/img-01.jfif" className="pic01"/>
-         < img src="/assets/photos/img-02.jfif" className="pic02"/>
-         < img src="/assets/photos/img-03.jfif" className="pic03"/>
-         < img src="/assets/photos/img-04.jfif" className="pic04"/>
-         < img src="/assets/photos/img-05.jfif" className='pic05'/>
+         lg:w-full lg:h-96 lg:pt-6  '>
+         < img src="/assets/photos/img-01.jfif" className="pic01 rounded-lg"/>
+         < img src="/assets/photos/img-02.jfif" className="pic02 rounded-lg"/>
+         < img src="/assets/photos/img-03.jfif" className="pic03 rounded-lg"/>
+         < img src="/assets/photos/img-04.jfif" className="pic04 rounded-lg"/>
+         < img src="/assets/photos/img-05.jfif" className='pic05 rounded-lg'/>
       </section>
    )
 }

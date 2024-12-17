32382e92efa972b93ee620907a7f95235e617874
Guna-Pandi <gunapandidurai@gmail.com>
2024-12-17 17:53:24 +0530

    Spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -4,13 +4,13 @@ const Spinner = () => {
   return (
     <div
       className=" h-[120px] w-[90px] 
-    absolute top-[15px]
-     -left-[40px] z-50 flex justify-center rounded-b-full p-3.5 bg-opacity-50 bg-white items-center lg:top-0 lg:left-4 "
+    absolute top-[0px]
+     left-[60px] z-50 flex justify-center rounded-b-full p-3.5 bg-opacity-50 bg-white items-center lg:top-0 lg:left-4 "
     >
       <img
         src="/assets/images/rounded-text.png"
         alt="Espiral animada"
-        className="hidden lg:flex  "
+        className="lg:flex  "
       />
     </div>
   );
